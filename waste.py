945124f2c499f40c86d7877ce511504cd816dd73
Klaus Umbach <klaus-github@uxix.de>
2012-08-07 00:04:28 +0200

    initial readonly CLI

---
+++ waste.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import urllib2
+
+class Client(object): # {{{
+    """docstring for client"""
+    def __init__(self, api_url):
+        super(Client, self).__init__()
+        self.api_url = api_url
+        self.tasks = []
+        self.task_filter = []
+
+    def get_tasks(self):
+        """docstring for get_tasks"""
+
+        url = '%s/get_tasks' % (self.api_url, )
+        self.tasks = eval(urllib2.urlopen(url).read())
+
+# }}}
+
+class Display(object): # {{{
+    """docstring for Display"""
+    def __init__(self, separator=' | '):
+        super(Display, self).__init__()
+        self.__items = ('id', 'title', 'status', )
+        self.__separator = separator
+
+    def show_tasks(self, tasks):
+        """docstring for show_tasks"""
+        items = self.__items
+        template = self.__get_task_template(tasks)
+
+        title = template % tuple( [ i.upper() for i in items ] )
+        print title
+        line = ''
+        for i in title:
+            line += '-'
+        print line
+    
+        for t in tasks:
+            data = []
+            for i in items:
+                data.append(t[i])
+            print template % tuple(data)
+
+    def __get_task_template(self,tasks): # {{{
+        """docstring for get_item_sizes"""
+
+        if len(tasks) == 0: return ''
+
+        items = self.__items
+        separator = self.__separator
+        max_size_item = {}
+        template = ''
+
+        for i in items:
+            max_size_item[i] = len(i)
+
+        for t in tasks:
+            for i in items:
+                if len(unicode(t[i])) > max_size_item[i]:
+                    max_size_item[i] = len(unicode(t[i]))
+
+        for i in items:
+            if tasks[0][i].__class__.__name__ != 'int':
+               item_format = '-'
+            else:
+               item_format = '+'
+
+            template += '%%%s%ss%s' % (item_format, max_size_item[i], separator,)
+
+        return template
+    # }}}
+
+
+
+    def set_filter(self, items):
+        """docstring for set_filter"""
+        self.__items = tuple(items)
+
+# }}}
+
+if __name__ == "__main__":
+
+    api_url='http://localhost:8080/api'
+
+    c = Client(api_url)
+    d = Display()
+    c.get_tasks()
+    d.show_tasks(c.tasks)
+
+# vim:fdm=marker:ts=4:sw=4:sts=4:ai:sta:et
