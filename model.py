bf0725ffcfc129f4c34c39b74fa11527f9310382
Klaus Umbach <klaus-github@uxix.de>
2012-08-09 15:55:59 +0200

    sync debug

--- model.py
+++ model.py
@@ -380,7 +380,7 @@ def solve_conflicts(conflict_dict): # {{{
             db.delete('Tasks', where='id=$RemoteID', vars=locals())
             x = db.insert('Tasks', **r)
             print "XXXXX %s XXXXX" % (x,)
-            l.pop(RemoteID)
+            l.pop('id')
             x = db.insert('Tasks', **l)
             print "XXXXX %s XXXXX" % (x,)
 
